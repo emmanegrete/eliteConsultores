b784fc0b653e7c65d80408682b8ca861fb8c022c
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-05-14 00:34:30 -0500

    se agregaron y corrigieron estilos
    1.-solucione el menu
    2.-las secciones ahora si son responsivas

--- index.php
+++ index.php
@@ -5,113 +5,160 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
 	<link rel="stylesheet" type="text/css" href="css/general.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
-		<div class="row-fluid">
-			<div class="span12">
-				<div class="color">
-					<nav class="navbar">
-						<div class="navbar-inner">
-							<div class="container-fluid">
-								<button class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse" type="button" >
-						            <span class="icon-bar"></span> 
-						            <span class="icon-bar"></span>
-						            <span class="icon-bar"></span>
-						        </button>
-								<div class="brand">
-									<div class="color">
-										logo
-									</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="color">
+				<nav class="navbar">
+					<div class="navbar-inner">
+						<div class="container-fluid">
+							<button class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse" type="button" >
+					            <span class="icon-bar"></span> 
+					            <span class="icon-bar"></span>
+					            <span class="icon-bar"></span>
+					        </button>
+							<div class="brand">
+								<div class="color">
+									logo
 								</div>
-								<div class="nav-collapse collapse">
-									<ul class="nav">
-										<li><a href="">uno</a></li>
-										<li><a href="">dos</a></li>
-										<li><a href="">tres</a></li>
-										<li><a href="">cuatro</a></li>
-										<li><a href="">cinco</a></li>
-										<li><a href="">seis</a></li>
-									</ul>
+							</div>
+							<div class="nav-collapse collapse">
+								<ul class="nav">
+									<li><a href="">uno</a></li>
+									<li><a href="">dos</a></li>
+									<li><a href="">tres</a></li>
+									<li><a href="">cuatro</a></li>
+									<li><a href="">cinco</a></li>
+									<li><a href="">seis</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide backgroundNaranja">
+					contenido
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="backgroundNaranja alCien">
+							<div class="padding20">
+								<p class="textoCenefa">holo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span7 offset1">
+						<div class="row-fluid">
+							<div class="span6">
+								<div class="color padding10 margin10">
+									soluciones
+								</div>
+							</div>
+							<div class="span6">
+								<div class="color padding10 margin10">
+									soluciones
 								</div>
 							</div>
 						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div class="span12">
-					<div data-stellar-background-ratio='0.5' class="color slide backgroundNaranja">
-						contenido
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
-					scroll
-				</div>
-			</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color">
-					<div class="backgroundNaranja alCien">
-						<div class="padding20">
-							<p class="textoCenefa"></p>
+						<div class="row-fluid">
+							<div class="span6">
+								<div class="color padding10 margin10">
+									boton3
+								</div>
+							</div>
+							<div class="span6">
+								<div class="color padding10 margin10">
+									boton4
+								</div>
+							</div>
 						</div>
 					</div>
+					<div class="span4">
+						
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					scroll
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="backgroundNaranja alCien">
+					<div class="padding20">
+						<p class="textoCenefa"></p>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					patatas
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="backgroundNaranja alCien">
+					<div class="padding20">
+						<p class="textoCenefa"></p>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					scroll
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="backgroundNaranja alCien">
+					<div class="padding20">
+						<p class="textoCenefa"></p>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					patatas
-				</div>
-			</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					scroll
-				</div>
-			</div>
-		</div>
-		<div class="row-fluid">
-			<div class="span12">
-				<div data-stellar-background-ratio='0.5' class="color slide">
-					patatas
-				</div>
-			</div>
-		</div>
+	</div>
 	<script src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
     <script type="text/javascript" src="js/modernizr.js"></script>
--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
 	$(window).stellar();
-	$('.pantallaCompleta').css({
-		'height':screen.availHeight + 'px';
-	});
 });
