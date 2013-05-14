01b0097040164905021fbac73d5734524009ad34
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-05-13 20:10:48 -0500

    paginaElite inicio

---
+++ index.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="css/general.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="color">
+					<nav class="navbar">
+						<div class="navbar-inner">
+							<div class="container-fluid">
+								<button class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse" type="button" >
+						            <span class="icon-bar"></span> 
+						            <span class="icon-bar"></span>
+						            <span class="icon-bar"></span>
+						        </button>
+								<div class="brand">
+									<div class="color">
+										logo
+									</div>
+								</div>
+								<div class="nav-collapse collapse">
+									<ul class="nav">
+										<li><a href="">uno</a></li>
+										<li><a href="">dos</a></li>
+										<li><a href="">tres</a></li>
+										<li><a href="">cuatro</a></li>
+										<li><a href="">cinco</a></li>
+										<li><a href="">seis</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="span12">
+					<div data-stellar-background-ratio='0.5' class="color slide backgroundNaranja">
+						contenido
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+					scroll
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color">
+					<div class="backgroundNaranja alCien">
+						<div class="padding20">
+							<p class="textoCenefa"></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					scroll
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					patatas
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					scroll
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					patatas
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					scroll
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span12">
+				<div data-stellar-background-ratio='0.5' class="color slide">
+					patatas
+				</div>
+			</div>
+		</div>
+	<script src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+    <script type="text/javascript" src="js/modernizr.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.stellar.min.js"></script>
+	<script type="text/javascript" src="js/principal.js"></script>
+</body>
+</html>
---
+++ js/principal.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(window).stellar();
+	$('.pantallaCompleta').css({
+		'height':screen.availHeight + 'px';
+	});
+});
