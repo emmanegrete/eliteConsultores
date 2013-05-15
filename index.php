6626f9c4056c0d13067e1693613341352a16d204
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-05-15 12:26:16 -0500

    cree el mapa y asi
    trololol 1

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/general.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" type="text/css" href="css/general.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -47,7 +47,13 @@
 		<div class="span12">
 			<div class="span12">
 				<div data-stellar-background-ratio='0.5' class="color slide backgroundNaranja">
-					contenido
+					<div class="somoElite">
+						<h1>SOMOS ÉLITE.</h1>
+						<p>Una firma de jóvenes líderes especialistas en el área <span>FISCAL-CONTABLE-FINANCIERA 
+						Y JURÍDICA</span></p>
+
+						<p>Con el firme propósito de apoyar y hacer crecer nuevos proyectos empresariales e impulsar y sostener proyectos ya establecidos en la región.</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -66,41 +72,199 @@
 					<div class="span12">
 						<div class="backgroundNaranja alCien">
 							<div class="padding20">
-								<p class="textoCenefa">holo</p>
+								<p class="textoCenefa">SERVICIOS AL <br>PROFESIONALES</p>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="row-fluid">
-					<div class="span7 offset1">
-						<div class="row-fluid">
-							<div class="span6">
-								<div class="color padding10 margin10">
-									soluciones
+					<div class="span12">
+						<div class="fondoCrema sombraCremaAdentro">
+							<div class="row-fluid">
+								<div class="span7 offset1">
+									<div class="row-fluid">
+										<div class="span6">
+											div.row-fluid>div.span12>div.
+											<div class="color padding10 margin10 sombraCremaAfuera seccionActiva">
+												SERVICIOS PROFESIONALES
+											</div>
+											<div class="imagenSolucion">
+												<img src="" alt="">
+											</div>
+										</div>
+										<div class="span6">
+											<div class="color padding10 margin10 sombraCremaAfuera">
+												SERVICIOS AL SECTOR PRIVADO
+											</div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span6">
+											<div class="color padding10 margin10 sombraCremaAfuera">
+												SERVICIOS AL SECTOR PÚBLICO
+											</div>
+										</div>
+										<div class="span6">
+											<div class="color padding10 margin10 sombraCremaAfuera">
+												NOSOTROS
+											</div>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="span6">
-								<div class="color padding10 margin10">
-									soluciones
+								<div class="span4">
+									
 								</div>
 							</div>
 						</div>
-						<div class="row-fluid">
-							<div class="span6">
-								<div class="color padding10 margin10">
-									boton3
-								</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="backgroundNaranja alCien">
+							<div class="padding20">
+								<p class="textoCenefa">LOCALÍZANOS</p>
 							</div>
-							<div class="span6">
-								<div class="color padding10 margin10">
-									boton4
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="mapaGoogle">
+							<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/?ie=UTF8&amp;t=m&amp;ll=21.14289,-101.687586&amp;spn=0.004803,0.006866&amp;z=17&amp;output=embed"></iframe>							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
+				scroll
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div data-stellar-background-ratio='0.5' class="color">
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="backgroundNaranja alCien">
+							<div class="padding20">
+								<p class="textoCenefa">CONTÁCTANOS</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="fondoCrema sombraCremaAdentro">
+							<div class="row-fluid">
+								<div class="span6">
+									<div class="row-fluid">
+										<div class="span11 offset1">
+											<div class="color">
+												<div class="titulo">Nombre</div>
+												<input type="text" name="nombreEnvio" class="nombreMail">
+											</div>
+											<div class="errorNombre"></div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span11 offset1">
+											<div class="color">
+												<div class="titulo">E-Mail</div>
+												<input type="email" name="emailEnvio" class="emailMail">
+											</div>
+											<div class="errorEmail"></div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span11 offset1">
+											<div class="color">
+												<div class="titulo">Asunto</div>
+												<input type="email" name="asuntoEnvio" class="emailMail">
+											</div>
+											<div class="errorAsunto"></div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span11 offset1">
+											<div class="color">
+												<div class="titulo">Mensaje</div>
+												<input type="text" name="mensajeEnvio" class="emailMail">
+											</div>
+											<div class="errorMensaje"></div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span11 offset1">
+											<div class="color">
+												<div class="row-fluid">
+													<div class="span6">
+														<p>Todos los campos son obligatorios</p>
+													</div>
+													<div class="span6">
+														<div class="botonEnvio">
+															Enviar
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									
+								</div>
+								<div class="span6">
+									<div class="row-fluid">
+										<div class="span12">
+											<div class="color">
+												<p>
+													Alud no. 902 Col. Jardines del Moral <br>
+													León, Gto, México <br>
+													c.p.37160
+												</p>
+											</div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span12">
+											<div class="color">
+												<p>
+													52 (477) 7 79 65 00<br>
+													52 (477) 3 11 75 25<br>
+													52 (477) 3 11 77 40
+												</p>
+											</div>
+										</div>
+									</div>
+									<div class="row-fluid">
+										<div class="span12">
+											<div class="color">
+												<p>
+													foo@eliteconsultores.com.mx
+												</p>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="span4">
-						
-					</div>
 				</div>
 			</div>
 		</div>
@@ -115,45 +279,92 @@
 	<div class="row-fluid">
 		<div class="span12">
 			<div data-stellar-background-ratio='0.5' class="color">
-				<div class="backgroundNaranja alCien">
-					<div class="padding20">
-						<p class="textoCenefa"></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12">
-			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
-				scroll
-			</div>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12">
-			<div data-stellar-background-ratio='0.5' class="color">
-				<div class="backgroundNaranja alCien">
-					<div class="padding20">
-						<p class="textoCenefa"></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12">
-			<div data-stellar-background-ratio='0.5' class="color slide fondoScroll">
-				scroll
-			</div>
-		</div>
-	</div>
-	<div class="row-fluid">
-		<div class="span12">
-			<div data-stellar-background-ratio='0.5' class="color">
-				<div class="backgroundNaranja alCien">
-					<div class="padding20">
-						<p class="textoCenefa"></p>
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="backgroundGris alCien sombraGrisAdentro">
+							<div class="padding20">
+								<div class="row-fluid">
+									<div class="span4">
+										<div class="row-fluid">
+											<div class="span4">
+												<div class="imagenIcono">
+													<div class="color"></div>
+												</div>
+											</div>
+											<div class="span4">
+												<div class="imagenIcono">
+													<div class="color"></div>
+												</div>
+											</div>
+											<div class="span4">
+												<div class="imagenIcono">
+													<div class="color"></div>
+												</div>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span12">
+												<div class="color">
+													<div>
+														52 (477) 7 79 65 00<br>
+														52 (477) 3 11 75 25<br>
+														52 (477) 3 11 77 40<br>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="span8">
+										<div class="row-fluid">
+											<div class="span3">
+												<div class="color">
+													<div>
+														SERVICIOS PROFESIONALES
+													</div>
+												</div>
+											</div>
+											<div class="span3">
+												<div class="color">
+													<div>
+														SERVICIOS AL SECTOR PRIVADO
+													</div>
+												</div>
+											</div>
+											<div class="span3">
+												<div class="color">
+													<div>
+														SERVICIOS AL SECTOR PÚBLICO
+													</div>
+												</div>
+											</div>
+											<div class="span3">
+												<div class="color">
+													<div>
+														NOSOTROS
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span3">
+												<div class="color">
+													<div>
+														LOCALÍZANOS
+													</div>
+												</div>
+											</div>
+											<div class="span3">
+												<div class="color">
+													<div>
+														CONTÁCTANOS
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>	
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
