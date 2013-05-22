03e968c15dd828ccd58e2bf0621f5e2d57328669
emmanuel negrete <emmanuel@emmanuelnegrete.net>
2013-05-22 11:21:45 -0500

    ya todos aparecen y desaparecen

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,43 @@
 $(document).ready(function(){
 	
+	function cargaInicio(identificador){
+		var nombreServ;
+		var servAct = $(".tituloServicio");
+		var cambioAjax = $(".ajaxCambio");
+		var direccion;
+		switch(identificador){
+			
+			case "servProfesionales":
+				direccion="servicio_Profesionales.html";
+				nombreServ="SERVICIOS PROFESIONALES.";
+			break;
+			case "servSectorPrivado":
+				direccion="servicios_al_sector_privado.html";
+				nombreServ="SERVICIOS AL SECTOR PRIVADO.";
+			break;
+			case "servSectorPublico":
+				direccion="servicios_al_sector_publico.html";
+				nombreServ="SERVICIOS AL SECTOR PÚBLICO.";
+			break;
+			case "nosotros":
+				direccion="nosotros.html";
+				nombreServ="NOSOTROS.";
+			break;
+		}
+
+		cambioAjax.fadeOut("fast",function(){
+				cambioAjax.load("cargaAjax/"+direccion,function(){
+					cambioAjax.fadeIn("fast", function(){
+						servAct.text(nombreServ);
+					});
+				});
+		});
+	}
+
 	$(window).stellar();
-	$(".inicioServ").on("click",function(){
-		$(".ajaxCambio").fadeOut('slow',function(){
-			$(this).load("cargaAjax/inicioServ.html",function(){
-				$(this).fadeIn("slow");
-			});
-		});		
-		$(".servicioActual").text("");
-	});
+
+	
+
 	/*-------- Menu Inicio --------*/
 	$(".servicioElite").on({
 		mouseenter:function(event){
@@ -38,39 +67,22 @@ $(document).ready(function(){
 				}	
 
 		},click:function(){ 
-			$(".inicioServ").on("click",function(){
-				$(".ajaxCambio").fadeOut('slow',function(){
-					$(this).load("cargaAjax/inicioServ.html",function(){
-						$(this).fadeIn("slow");
-					});
-				});		
-				$(".servicioActual").text("");
-			});
-			var idServicio = $(this).attr("id");
-			switch(idServicio){
-
-					case "servProfesionales":
-						$(".ajaxCambio").load("cargaAjax/servicio_Profesionales.html", function() {
-							$(".servicioActual").text("Asesoría y Planeación Fiscal.");
+			
+			$(".inicioServ").on({
+				mouseenter:function(){
+					$(this).css({"cursor":"pointer"});
+				},click:function(){
+					$(".ajaxCambio").fadeOut('fast',function(){
+						$(this).load("cargaAjax/inicioServ.html",function(){
+							$(this).fadeIn("fast");
 						});
-					break;
-					case "servSectorPrivado":
-						$(".ajaxCambio").load("cargaAjax/servicios_al_sector_privado.html", function() {
-							
-							$(".servicioActual").text("Auditoría Externa.");
-						});
-					break;
-					case "servSectorPublico":
-					 	$(".ajaxCambio").load("cargaAjax/servicios_al_sector_publico.html", function() {
-							$(".servicioActual").text("Planeación.");
-						});
-					break;
-					case "nosotros":
-						$(".ajaxCambio").load("cargaAjax/nosotros.html", function() {
-							$(".servicioActual").text("Antecedentes.");
-						});
-					break;
+					});		
+					$(".servicioActual").text("");
 				}
+			});
+
+			var idServicio = $(this).attr("id");
+			cargaInicio(idServicio);
 		 }
 	});
 	
